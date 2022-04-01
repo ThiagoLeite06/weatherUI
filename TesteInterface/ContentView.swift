25bc8496a5a3535b8ab62a553e6f51dcc4072a98
thiagoleite6 <thiago.0686@gmail.com>
2022-04-01 19:08:07 -0300

    Initial Commit

---
+++ TesteInterface/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TesteInterface
+//
+//  Created by Thiago Leite on 01/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TesteInterface/TesteInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteInterfaceApp.swift
+//  TesteInterface
+//
+//  Created by Thiago Leite on 01/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TesteInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
