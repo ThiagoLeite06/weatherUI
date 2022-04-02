10691bb20ad575065ec5486e40748eb9e02a12d6
thiagoleite6 <thiago.0686@gmail.com>
2022-04-01 23:43:08 -0300

    First commit

--- TesteInterface/TesteInterfaceApp.swift
+++ TesteInterface/TesteInterfaceApp.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 @main
 struct TesteInterfaceApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                print("App Ativo")
+            case .inactive:
+                print("App Inativo")
+            case .background:
+                print("App em Background")
+                
+            @unknown default:
+                print("What!?")
+                
+            }
         }
     }
 }
